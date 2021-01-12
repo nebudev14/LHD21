6daf1cb3b0a43f73dd2269e6771cd7c7317a1e34
warren yun <xxwarrenhdxx@gmail.com>
2021-01-11 21:48:45 -0500

    Local Hack Day 2, Music API

---
+++ SpotifyApi.py
@@ -0,0 +1,27 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+cid = 'eb277b17ef36405a8d47aae6dd27d276'
+secret = 'e29bd49d11874fcfae23a9cf1298df74'
+
+
+
+artist_uri = input("Enter in the Spotify Artist Link to get statistics about that artist: ")
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=cid, client_secret=secret))
+
+
+album_results = spotify.artist_albums(artist_uri, album_type='album')
+albums = album_results['items']
+while album_results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+print(spotify.artist(artist_uri)['name'] + ": \n----------\n")
+print("Follower Count : " + str(spotify.artist(artist_uri)['followers']['total']) + "\n----------\n")
+print("Albums: \n----------")
+for album in albums:
+    print(album['name'])
+print("Top 10 Tracks: \n----------\n")
+top10 = spotify.artist_top_tracks(artist_uri)
+for track in top10['tracks'][:10]:
+    print("-" + track['name'])
